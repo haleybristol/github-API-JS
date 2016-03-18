4d5d17c3fa86690bf89328809255e059860ad981
haley bristol <hebristol@gmail.com>
2016-03-18 15:58:11 -0700

    add styles

--- js/back-end.js
+++ js/back-end.js
@@ -7,13 +7,14 @@ exports.getRepos = function(user){
     // var followers = response.followers;
     // var repos = response[].archive_url;
     for (var i = 0; i < response.length; i += 1) {
-      $('#name').append('<div class="title"><strong>Repository:</strong> ' + response[i].name +'</div>' + '<div class="description"><strong>Description: </strong> ' + response[i].description + '</div><br>');
+      $('#userInfo').append('<div class="row"><strong>Repository:</strong> ' + response[i].name + '<div class="col-xs-6"><strong>Description: </strong> ' + response[i].description + '</div></div><br>');
     }
   });
     $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
       console.log(response);
-      $('#email').html('<div class="email"><strong>E-mail: '+ response.email + '</strong></div>');
-      $('#followers').html('<div class="follow"><strong> followers: ' + response.followers +'</strong></div>');
+      $('#name').html('<div class="name"><strong>Name: ' + response.name + '</strong></div>');
+      $('#email').html('<div class="email"><strong>E-mail: ' + response.email + '</strong></div>');
+      $('#followers').html('<div class="follow"><strong> Followers: ' + response.followers +'</strong></div><br>');
     })
 
     // $('#email').html(email);
