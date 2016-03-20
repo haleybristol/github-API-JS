022b667805b81a58ec8630cc47939be73dcc687f
haley bristol <hebristol@gmail.com>
2016-03-19 21:25:31 -0700

    call getRepos function

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -4,5 +4,6 @@ var getRepos = require('./../js/back-end.js').getRepos;
 $(document).ready(function(){
   $('#locateUser').submit(function(event){
     event.preventDefault();
+    getRepos();
   })
 })
